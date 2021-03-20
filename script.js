2eee47f58f9104499477b55183553b1b21a5e31f
felixhal <felix240600@gmail.com>
2021-03-20 23:26:30 +0700

    Added Email, Pass & Confirm Pass Validation

--- script.js
+++ script.js
@@ -11,14 +11,18 @@ let showError = (input, message) => {
   small.innerText = message;
 };
 
-let success = (input) => {
+let showSuccess = (input) => {
   const formControl = input.parentElement;
   formControl.className = 'form-control success';
 };
 
-let isEmail = (email) => {
+let checkEmail = (input) => {
   const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  return re.test(String(email).toLowerCase());
+  if (re.test(input.value.trim())) {
+    showSuccess(input);
+  } else {
+    showError(input, 'Email is not valid');
+  }
 };
 
 let checkRequired = (inputArr) => {
@@ -31,6 +35,28 @@ let checkRequired = (inputArr) => {
   });
 };
 
+let checkConfirmPassword = (pass, confirmPass) => {
+  if (pass.value.trim() == confirmPass.value.trim()) {
+    showSuccess(pass);
+  } else {
+    showError(confirmPass, 'Password does not match');
+  }
+};
+
+let checkLength = (input, min, max) => {
+  if (input.value.length < min) {
+    showError(
+      input,
+      `${getFieldName(input)} must be at least ${min} characters`
+    );
+  } else if (input.value.length > max) {
+    showError(
+      input,
+      `${getFieldName(input)} must be less than ${max} characters`
+    );
+  }
+};
+
 let getFieldName = (input) => {
   return input.id.charAt(0).toLowerCase() + input.id.slice(1);
 };
@@ -40,4 +66,8 @@ form.addEventListener('submit', function (e) {
   e.preventDefault();
 
   checkRequired([username, email, password, password2]);
+  checkLength(username, 3, 15);
+  checkLength(password, 6, 25);
+  checkEmail(email);
+  checkConfirmPassword(password, password2);
 });
