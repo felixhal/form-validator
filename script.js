c4df08af27f0228916d84c7b04d341b8cfc9b59e
felixhal <felix240600@gmail.com>
2021-03-20 23:13:12 +0700

    Added isRequired validation Script

---
+++ script.js
@@ -0,0 +1,43 @@
+const form = document.getElementById('form');
+const username = document.getElementById('username');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const password2 = document.getElementById('password2');
+
+let showError = (input, message) => {
+  const formControl = input.parentElement;
+  formControl.className = 'form-control error';
+  const small = formControl.querySelector('small');
+  small.innerText = message;
+};
+
+let success = (input) => {
+  const formControl = input.parentElement;
+  formControl.className = 'form-control success';
+};
+
+let isEmail = (email) => {
+  const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
+};
+
+let checkRequired = (inputArr) => {
+  inputArr.forEach((input) => {
+    if (input.value.trim() === '') {
+      showError(input, `${getFieldName(input)} is required`);
+    } else {
+      showSuccess(input);
+    }
+  });
+};
+
+let getFieldName = (input) => {
+  return input.id.charAt(0).toLowerCase() + input.id.slice(1);
+};
+
+//Event Listeners
+form.addEventListener('submit', function (e) {
+  e.preventDefault();
+
+  checkRequired([username, email, password, password2]);
+});
